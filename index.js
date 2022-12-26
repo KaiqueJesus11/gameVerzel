516fbfb53569e6b5134b289940d7f9c128d7d3c5
KaiqueJesus11 <39889431+KaiqueJesus11@users.noreply.github.com>
2022-12-26 18:51:43 -0300

    commit initial

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const sonic = () => {
+    window.location.href = "sonic.html";
+}
+
+const alexKidd = () => {
+    window.location.href = "alex_kidd.html";
+}
+
+const bomberMan = () => {
+    window.location.href = "bomber_man.html";
+}
+
+const crashTeamRacing = () => {
+    window.location.href = "crash_team_racing.html";
+}
+
+const marioKart = () => {
+    window.location.href = "mario_kart.html";
+}
+
+const marvelVsCampcom = () => {
+    window.location.href = "marvel_vs_capcom.html";
+}
+
+const sinuca = () => {
+    window.location.href = "sinuca.html";
+}
+
+const streetFighter = () => {
+    window.location.href = "streetFighter.html";
+}
+
+const tekken = () => {
+    window.location.href = "tekken.html";
+}
+
+const topGear = () => {
+    window.location.href = "top_gear.html";
+}
